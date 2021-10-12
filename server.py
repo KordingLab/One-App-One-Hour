31c758224e8ed5d758fb6f4825960b5e08ac0075
Jordan Matelsky <j6k4m8@gmail.com>
2021-10-12 16:24:01 -0400

    Add initial commit from Oct 12 lab teaching

---
+++ server.py
@@ -0,0 +1,33 @@
+
+from flask import Flask, jsonify, request, render_template
+import json
+import time
+import glob
+
+App = Flask(__name__)
+
+@App.route("/stats")
+def stats():
+    data_files = glob.glob("data-*.json")
+    data = [json.load(open(d, 'r')) for d in data_files]
+    return jsonify({"data": data})
+
+
+@App.route("/upload-data", methods=["POST"])
+def upload_data():
+    json.dump(
+        request.json,
+        open(f"data-{time.time()}.json", 'w')
+    )
+    return jsonify({
+        "successful": "yes!"
+    })
+
+
+@App.route("/")
+def homepage():
+    return render_template("index.html")
+
+
+
+App.run(debug=True)
